9b7fe5967b654af2aee02d95ab2733564b53ad44
Vaibhav Tyagi <VAIBHAVTYAGI438@GMAIL.COM>
2022-12-07 12:18:29 +0530

    setup completed

---
+++ frontend_social_circle/src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div>
+      <h1 className="bg-rose-600 text-lg underline">Hi! I am Vaibhav!</h1>
+    </div>
+  );
+};
+
+export default App;
